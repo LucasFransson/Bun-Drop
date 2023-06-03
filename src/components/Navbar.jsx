899c43dbe9ec828598a6c24747804ac427defbac
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-06-03 02:24:10 +0200

    Browser refactoring attemp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 function Navbar() {
 	return (
 		<>
-			<nav className="navbar"></nav>
+			<nav className="navbar">
+				<ul>
+					<li>
+						<Link to={'/menu'}>
+							<button>Menu</button>
+						</Link>
+					</li>
+					<li>Home</li>
+					<li>Search</li>
+					<li>Cart</li>
+					<li>Settings</li>
+					<li>User</li>
+				</ul>
+			</nav>
 		</>
 	);
 }
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,7 +4,6 @@ function NotFoundPage() {
 	return (
 		<>
 			<h1>404: Not Found</h1>
-			// Create a button that redirects the user to the home page
 			<p>Oops! Something went wrong.</p>
 			<button>Go to Home Page</button>
 		</>
