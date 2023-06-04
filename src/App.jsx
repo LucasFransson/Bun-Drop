c0ba6b1a20f6197558819ef5f467bf97cb03d773
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-06-04 21:36:51 +0200

    Complete & Total UI Rework - initial new ui

--- src/App.jsx
+++ src/App.jsx
@@ -23,33 +23,3 @@ function App() {
 }
 
 export default App;
-
-{
-	/* <div className="cards">
-				{items.map((item) => (
-					<Card
-						key={item.id}
-						name={item.name}
-						image={item.image}
-						category={item.category}
-						price={item.price}
-					/>
-				))}
-			</div> */
-}
-
-// import './App.css';
-// import './hooks/useFetch';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Button from './components/button.jsx';
-
-// function App() {
-// 	return (
-// 		<>
-
-// 			const items = useFetch('http://localhost:7000/burgers', []);
-// 		</>
-// 	);
-// }
-
-// export default App;
